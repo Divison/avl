c7737412951744108be0d34372a5b9f1fbaae24a
Divison <no1@whatever.com>
2017-04-10 02:47:43 +0300

    new

---
+++ gitcheck/gitcheck/gitcheck.cpp
@@ -0,0 +1,15 @@
+// gitcheck.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "conio.h"
+#include "iostream"
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << " hello world " << endl;
+	_getch();
+	return 0;
+}
+
