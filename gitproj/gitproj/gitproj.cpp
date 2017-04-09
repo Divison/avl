ee0de5a76bbce94af3018e54ef5fec6b347ea71a
Divison <no@whatever.com>
2017-04-10 02:34:39 +0300

    123

---
+++ gitproj/gitproj/gitproj.cpp
@@ -0,0 +1,15 @@
+// gitproj.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include "conio.h"
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "hello world" << endl;
+	_getch();
+	return 0;
+}
+
